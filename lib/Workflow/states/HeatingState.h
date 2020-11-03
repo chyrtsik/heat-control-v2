b89e109edc66eadf0050a444e2aa3a1714211137
Aliaksandr Chyrtsik <tchirtik@gmail.com>
2020-11-03 21:44:21 +0300

    Fix heating settings and use less loaded line by default.

--- lib/Workflow/states/HeatingState.h
+++ lib/Workflow/states/HeatingState.h
@@ -13,7 +13,7 @@
 
 #define SECOND_HEATER_ENGAGE_TEMPERATURE -10.0
 
-#define HEATER_MAX_TEMPERATURE  50.0  //Max working temperature for electric heater
+#define HEATER_MAX_TEMPERATURE  55.0  //Max working temperature for electric heater
 #define HEATER_MIN_TEMPERATURE  30.0  //Min working temperature for electric heater
 #define HEATER_DELTA_TEMPERATURE 2.5  //Delta around working heater temperature which is allowed
 
