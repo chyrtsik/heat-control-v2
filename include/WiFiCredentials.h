463418b42b5c58e6424c2a22507e52443926342b
Aliaksandr Chyrtsik <aliaksandrchyrtsik@cloud.upwork.com>
2022-01-23 22:37:28 +0200

    Remove hardcoded wifi credentials.

--- include/WiFiCredentials.h
+++ include/WiFiCredentials.h
@@ -1,3 +1,3 @@
 //These credentials are to be stored outside or the project, so that they are not hardcoded
-const char *ssid = "Sensors WiFi";              //special hidden network for sensors
-const char *password = "1ewr4sfdeh823y48g234";  //password 
+const char *ssid = WIFI_SSID;      
+const char *password = WIFI_PASS;  
